c9ce8bcfd4567e1de04e35e72b15727847768cf7
monkut <shane.cousins@gmail.com>
2021-12-17 18:49:42 +0900

    :wrench: refactor remove usage of 'rx' replace with thread monitor

--- igata/checkers/aws/spot.py
+++ igata/checkers/aws/spot.py
@@ -1,15 +1,20 @@
-import rx
-import rx.operators as ops
+import logging
+
+from time import sleep
+from threading import Thread
+from typing import NamedTuple
 from requests import Response
 
 from ...utils import requests_retry_session
-from .observers import CheckerMessage
+
+
+logger = logging.getLogger("checkers")
 
 INSTANCE_ACTION_URL = "http://169.254.169.254/latest/meta-data/spot/instance-action"
 SPOT_INSTANCE_CHECKER_TYPE = "spot_instance"
 
 
-def access_spot_instance_state(_) -> Response:
+def access_spot_instance_state() -> Response:
     """
     Obtain the spot instance state
 
@@ -21,25 +26,42 @@ def access_spot_instance_state(_) -> Response:
         {"action": "stop", "time": "2017-09-18T08:22:00Z"}
 
     """
-    return requests_retry_session().get(INSTANCE_ACTION_URL)
+    retry_session = requests_retry_session()
+    response = retry_session.get(INSTANCE_ACTION_URL)
+    return response
 
 
-def spot_instance_state_response_to_checker_message(resp: Response) -> CheckerMessage:
-    """
-    Handle the access_spot_instance_state response
-    """
-    message = resp.json()
-    return CheckerMessage(SPOT_INSTANCE_CHECKER_TYPE, f'"spot/instance-action": {message}')
+class CheckerMessage(NamedTuple):
+    """Checker message"""
+
+    checker_type: str
+    body: str
 
 
-def spot_instance_check_observable(interval_seconds: float = 1.0) -> rx.Observable:
-    """
-    スポットインスタンスのステータスをinterval秒ごとにurlを叩いてチェックしに行くobservableを返す関数
-    :param interval_seconds: interval seconds
-    """
-    return rx.interval(interval_seconds).pipe(
-        ops.map(access_spot_instance_state),
-        ops.filter(lambda resp: resp.status_code != 404),
-        ops.map(spot_instance_state_response_to_checker_message),
-        ops.distinct_until_changed(),
-    )
+class SpotInstanceValueObserver(Thread):
+
+    def __init__(self, interval_seconds: float = 1.0):
+        Thread.__init__(self)
+        self.internal_seconds = interval_seconds
+        self.last_instance_state = None
+        self.started = False
+        self._terminate = False
+
+    def terminate(self):
+        self._terminate = True
+        if self.started:
+            self.join()
+
+    def run(self):
+        self.started = True
+        while True:
+            response = access_spot_instance_state()
+            if response.status_code != 404:
+                result = response.json()
+                message = CheckerMessage(SPOT_INSTANCE_CHECKER_TYPE, f'"spot/instance-action": {result}')
+                if message.checker_type == SPOT_INSTANCE_CHECKER_TYPE and self.last_instance_state != message.body:
+                    logger.info(f'Observer Change Detected checker_type("{message.checker_type}"): {message.body}')
+                self.last_instance_state = message.body
+            if self._terminate:
+                break
+            sleep(self.internal_seconds)
--- igata/cli.py
+++ igata/cli.py
@@ -6,14 +6,9 @@ from collections import Counter
 from pathlib import Path
 from typing import List, Type
 
-import rx
-import rx.operators as ops
-from rx.scheduler import ThreadPoolScheduler
-
 from . import __version__, settings
 from .checkers.aws.ec2 import get_instance_type
-from .checkers.aws.observers import CheckersObserver
-from .checkers.aws.spot import spot_instance_check_observable
+from .checkers.aws.spot import SpotInstanceValueObserver
 from .defaults import DEFAULT_MODEL_VERSION
 from .handlers import (
     INPUT_CONTEXT_MANAGER_REQUIRED_ENVARS,
@@ -207,23 +202,20 @@ if __name__ == "__main__":
     for setting in ctxmgr_settings:
         logger.info(f"{setting}: {ctxmgr_settings[setting]}")
 
-    scheduler = ThreadPoolScheduler(max_workers=1)
-    checkers_observable = rx.empty()
-
+    instance_observer = None
     if settings.INSTANCE_ON_AWS:
         logger.info(f"instance_type: {get_instance_type()}")  # Assumes being run on AWS EC2 instance
         if args.is_spot_instance or settings.AWS_ENABLE_SPOTINSTANCE_STATE_LOGGING:
             logger.info("Start spot_instance_observable monitoring...")
-            spot_instance_observable = spot_instance_check_observable()
-            checkers_observable = checkers_observable.pipe(ops.merge(spot_instance_observable))
+            instance_observer = SpotInstanceValueObserver(interval_seconds=1.0)
     elif args.is_spot_instance or settings.AWS_ENABLE_SPOTINSTANCE_STATE_LOGGING:
         logger.warning(
             '"--spot-instance" flag or AWS_ENABLE_SPOTINSTANCE_STATE_LOGGING envar given, '
             "but INSTANCE_ON_AWS == False, logging NOT performed!"
         )
 
-    checkers_observable.pipe(ops.publish()).connect(scheduler=scheduler)
-    checkers_observable.subscribe(CheckersObserver())
+    if instance_observer:
+        instance_observer.start()
 
     input_values = None
     if args.inputs:
@@ -238,3 +230,5 @@ if __name__ == "__main__":
         inputs=input_values,
     )
     logger.info(f"execution summary: {summary}")
+    if instance_observer:
+        instance_observer.terminate()
